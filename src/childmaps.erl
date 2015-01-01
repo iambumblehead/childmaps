62520cf5bfc35155b2f4448cf73bfbb0a3ce9511
chris <chris@bumblehead.com>
2015-01-01 02:24:51 -0800

    removed comments (no longer valid)

--- src/childmaps.erl
+++ src/childmaps.erl
@@ -5,9 +5,6 @@
 -export([to_list/1]).
 -export([converted/3]).
 
-%% @doc Returns a new map created from the old map with named properties
-%% converted from map to list.
-
 converted(Map, Val, Name) when is_map(Map) and is_map(Val) ->
     maps:update(Name, maps:to_list(Val), Map);
 converted(Map, Val, Name) when is_map(Map) and is_list(Val) ->  
@@ -28,6 +25,3 @@ to_list([Head|Rest]) ->
 
 to_list(Map) when is_map(Map) ->
     maps:to_list(to_list(maps:keys(Map), Map)).
-    
-    
-
